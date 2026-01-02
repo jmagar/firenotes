e504174474182d3264f2ff7e301e8adbce3837ac
Abimael Martell <abimex@gmail.com>
2026-01-02 11:03:49 -0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+/**
+ * Firecrawl CLI
+ * Entry point for the CLI application
+ */
+
+function main() {
+  console.log('Firecrawl CLI');
+  // Add your CLI logic here
+}
+
+if (require.main === module) {
+  main();
+}
+
+export default main;
+
